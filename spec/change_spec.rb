e7b13fc72cd9c1b914bf97c13c4f38a95008dd92
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 18:09:46 +0000

    DRYs out code using let block

--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -1,14 +1,13 @@
 require 'change'
 
 describe Change do
+  let(:change) { described_class.new }
   describe '#initialize' do
     it 'has a coins attribute that is an array' do
-      change = Change.new
       expect(change.coins).to be_an(Array)
     end
 
     it 'has coins that are an instance of the Coin class' do
-      change = Change.new
       expect(change.coins[4]).to be_an_instance_of(Coin)
     end
   end
