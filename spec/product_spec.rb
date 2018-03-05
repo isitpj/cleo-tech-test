c75c5ee2065e1fa46618520332068f1b5e063fd6
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 10:42:18 +0000

    DRYs out code using let statement

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,14 +1,14 @@
 require 'product'
 
 describe Product do
+  let(:kitkat) { described_class.new('Kit Kat', 90)}
+
   describe '#initialize' do
     it 'has a name property' do
-      kitkat = Product.new('Kit Kat', 90)
       expect(kitkat.name).to eq 'Kit Kat'
     end
 
     it 'has a price property' do
-      kitkat = Product.new('Kit Kat', 90)
       expect(kitkat.price).to eq 90
     end
   end
