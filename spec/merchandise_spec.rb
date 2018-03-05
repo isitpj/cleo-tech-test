d877d80fa5526352339732b4916edba5ceb5d1c7
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 10:09:53 +0000

    Runs SimpleCov alongside RSpec

--- spec/merchandise_spec.rb
+++ spec/merchandise_spec.rb
@@ -1,7 +1,7 @@
 require 'merchandise'
 
 describe Merchandise do
-  describe '#init' do
+  describe '#initialize' do
     it 'has a products attribute that is an array' do
       merchandise = Merchandise.new
       expect(merchandise.products).to be_an(Array)
