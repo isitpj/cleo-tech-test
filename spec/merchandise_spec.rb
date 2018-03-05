d0a7a71a5b44649a7a464bc841d77888bdb34085
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 11:55:51 +0000

    Extracts instantiation of new merchandise object to let block

--- spec/merchandise_spec.rb
+++ spec/merchandise_spec.rb
@@ -2,15 +2,16 @@ require 'merchandise'
 require 'product'
 
 describe Merchandise do
+  let(:merchandise) { described_class.new }
+
   describe '#initialize' do
     it 'has a products attribute that is an array' do
-      merchandise = Merchandise.new
       expect(merchandise.products).to be_an(Array)
     end
 
     it 'has products that are an instance of the Product class' do
-      merchandise = Merchandise.new
       expect(merchandise.products[4]).to be_an_instance_of(Product)
     end
   end
+
 end
