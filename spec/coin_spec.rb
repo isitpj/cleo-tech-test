2e798eba98ffb8d6316a66b696170148c4343cf4
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 16:45:09 +0000

    Tests coin quantity getter and that it is conditionally assigned to 20

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -6,5 +6,10 @@ describe Coin do
       coin = Coin.new(50)
       expect(coin.value).to be_an_instance_of(Integer)
     end
+
+    it 'has a quantity property that is assigned to 20 by default' do
+      coin = Coin.new(50)
+      expect(coin.quantity).to eq 20
+    end
   end
 end
