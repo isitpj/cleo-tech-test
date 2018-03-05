0557cb3d90ad0a0ab3004e9ab3b67cfa9aa0fc46
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 16:30:00 +0000

    Tests that #print_products calls #puts

--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -7,4 +7,13 @@ describe Machine do
       expect(machine.merchandise).to be_an_instance_of(Merchandise)
     end
   end
+
+  describe '#print_products' do
+    let(:machine) { described_class.new }
+    it 'calls puts on STDOUT' do
+      allow(STDOUT).to receive(:puts)
+      machine.print_products
+      expect(STDOUT).to have_received(:puts).at_least(1).times
+    end
+  end
 end
