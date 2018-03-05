ace270dee2175388099f03d6d55dc33ee1ef5e24
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 18:02:40 +0000

    Initialises coins attribute with an array of Coin objects

--- lib/change.rb
+++ lib/change.rb
@@ -2,6 +2,15 @@ class Change
   attr_reader :coins
 
   def initialize
-    @coins = []
+    @coins = [
+      Coin.new(1),
+      Coin.new(2),
+      Coin.new(5),
+      Coin.new(10),
+      Coin.new(20),
+      Coin.new(50),
+      Coin.new(100),
+      Coin.new(200)
+    ]
   end
 end
