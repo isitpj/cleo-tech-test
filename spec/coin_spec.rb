f91df6bc927f036556700efc3d8fe0c7fcf2e93b
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 16:46:04 +0000

    Extracts Coin instantiation to let block

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -1,14 +1,13 @@
 require 'coin'
 
 describe Coin do
+  let(:coin) { described_class.new(50) }
   describe '#initialize' do
     it 'has a value property that is an integer' do
-      coin = Coin.new(50)
       expect(coin.value).to be_an_instance_of(Integer)
     end
 
     it 'has a quantity property that is assigned to 20 by default' do
-      coin = Coin.new(50)
       expect(coin.quantity).to eq 20
     end
   end
