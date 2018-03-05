84b50b29ea47c8fc4f4158148383e24fcd364629
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 10:40:04 +0000

    Adds test for price property

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,8 +3,13 @@ require 'product'
 describe Product do
   describe '#initialize' do
     it 'has a name property' do
-      kitkat = Product.new('Kit Kat')
+      kitkat = Product.new('Kit Kat', 90)
       expect(kitkat.name).to eq 'Kit Kat'
+    end
+
+    it 'has a price property' do
+      kitkat = Product.new('Kit Kat', 90)
+      expect(kitkat.price).to eq 90
     end
   end
 end
