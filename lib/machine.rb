d4a386ec9650a8752e578e61e7c9e753ca27e328
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 16:29:15 +0000

    Adds method to display products and prices

--- lib/machine.rb
+++ lib/machine.rb
@@ -4,4 +4,10 @@ class Machine
   def initialize
     @merchandise = Merchandise.new
   end
+
+  def print_products
+    @merchandise.products.each_with_index do |product, index|
+      puts "#{index + 1}. #{product.name}. Price: #{product.price}"
+    end
+  end
 end
