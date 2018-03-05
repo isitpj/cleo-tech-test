a42a1ffc4651cfeb2fe7b47792eb3d19db8553b2
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 11:44:45 +0000

    Creates a default array of products

--- lib/merchandise.rb
+++ lib/merchandise.rb
@@ -1,9 +1,20 @@
+require 'product'
+
 class Merchandise
 
   attr_reader :products
 
   def initialize
-    @products = []
+    @products = [
+      Product.new('Kit Kat', 90, 10),
+      Product.new('Daiy Milk', 80, 10),
+      Product.new('Walkers Salt and Vinegar Crisps', 110, 10),
+      Product.new('Walkers Cheese and Onion Crisps', 110, 10),
+      Product.new('Coke', 180, 10),
+      Product.new('Diet Coke', 180, 10),
+      Product.new('Pepsi', 150, 10),
+      Product.new('Diet Pepsi', 150, 10)
+    ]
   end
 
 end
