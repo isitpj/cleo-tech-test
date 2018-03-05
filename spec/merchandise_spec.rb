8824001b374b8a5ba241330aa65247dd3a2c39d1
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 11:45:12 +0000

    Tests that items in Merchandise#products are instances of the Product class

--- spec/merchandise_spec.rb
+++ spec/merchandise_spec.rb
@@ -1,10 +1,16 @@
 require 'merchandise'
+require 'product'
 
 describe Merchandise do
   describe '#initialize' do
     it 'has a products attribute that is an array' do
       merchandise = Merchandise.new
       expect(merchandise.products).to be_an(Array)
+    end
+
+    it 'has products that are an instance of the Product class' do
+      merchandise = Merchandise.new
+      expect(merchandise.products[4]).to be_an_instance_of(Product)
     end
   end
 end
