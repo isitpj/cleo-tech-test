46796afca6428c61b985808bed26d84b305a9991
Peter Johnstone <peter.wdj@gmail.com>
2018-03-05 11:48:14 +0000

    Conditionally assigns product quantity attribute to 10

--- lib/product.rb
+++ lib/product.rb
@@ -2,7 +2,7 @@ class Product
 
   attr_reader :name, :price, :quantity
 
-  def initialize(name, price, quantity)
+  def initialize(name, price, quantity=10)
     @name = name
     @price = price
     @quantity = quantity
