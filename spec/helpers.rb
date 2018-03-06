e4585a860cc35794fda5eef71876e7e16594059d
Peter Johnstone <peter.wdj@gmail.com>
2018-03-06 09:30:32 +0000

    Creates #stub_generate_coins helper

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -3,3 +3,10 @@ def stub_generate_products(product)
   allow_any_instance_of(Merchandise).to receive(:generate_products) { mock_products }
   Merchandise.new
 end
+
+def stub_generate_coins(coin)
+  allow(coin).to receive(:value) { 1 }
+  mock_coins = [coin]
+  allow_any_instance_of(Change).to receive(:generate_coins) { mock_coins }
+  Change.new
+end
