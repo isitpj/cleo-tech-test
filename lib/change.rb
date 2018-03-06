e73ba77b9694e2263ab008fcc9c3345edb974a2e
Peter Johnstone <peter.wdj@gmail.com>
2018-03-06 15:20:10 +0000

    Put coins in descending reather than ascending order

--- lib/change.rb
+++ lib/change.rb
@@ -1,3 +1,5 @@
+require 'coin'
+
 class Change
   attr_reader :coins
 
@@ -27,14 +29,14 @@ class Change
 
   def generate_coins
     [
-      Coin.new(1),
-      Coin.new(2),
-      Coin.new(5),
-      Coin.new(10),
-      Coin.new(20),
-      Coin.new(50),
+      Coin.new(200),
       Coin.new(100),
-      Coin.new(200)
+      Coin.new(50),
+      Coin.new(20),
+      Coin.new(10),
+      Coin.new(5),
+      Coin.new(2),
+      Coin.new(1)
     ]
   end
 end
