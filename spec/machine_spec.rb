6b2598943b01a45753fc1b402efd19b5473e38f8
Peter Johnstone <peter.wdj@gmail.com>
2018-03-06 10:36:04 +0000

    Tests that @user_selection is assigned the user's choice of tasty treat

--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -1,4 +1,5 @@
 require 'machine'
+require 'merchandise'
 
 describe Machine do
   let(:machine) { described_class.new }
@@ -18,6 +19,14 @@ describe Machine do
       allow(STDOUT).to receive(:puts)
       machine.print_products
       expect(STDOUT).to have_received(:puts).at_least(1).times
+    end
+  end
+
+  describe '#process_user_selection' do
+    it 'assigns the @user_selection variable to a product' do
+      allow(STDIN).to receive(:gets) { '1' }
+      machine.process_user_selection
+      expect(machine.user_selection.name).to eq 'Dairy Milk'
     end
   end
 end
