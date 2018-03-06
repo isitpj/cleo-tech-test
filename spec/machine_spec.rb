297456808051ee5107ca40d7588829caf143f79e
Peter Johnstone <peter.wdj@gmail.com>
2018-03-06 10:26:35 +0000

    Tests that @user_selection is nil by default

--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -2,10 +2,14 @@ require 'machine'
 
 describe Machine do
   let(:machine) { described_class.new }
-  
+
   describe '#initialize' do
     it 'has a merchandise property that is an instance of Merchandise' do
       expect(machine.merchandise).to be_an_instance_of(Merchandise)
+    end
+
+    it 'has a user_selection property that is nil by default' do
+      expect(machine).to have_attributes(:user_selection => nil)
     end
   end
 
