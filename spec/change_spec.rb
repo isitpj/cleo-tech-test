94c281006405989e7357ad424842a83dce480616
Peter Johnstone <peter.wdj@gmail.com>
2018-03-06 09:29:12 +0000

    Tests #release_coin calls a coin's release method

--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -1,4 +1,5 @@
 require 'change'
+require 'coin'
 
 describe Change do
   let(:change) { described_class.new }
@@ -9,6 +10,19 @@ describe Change do
 
     it 'has coins that are an instance of the Coin class' do
       expect(change.coins[4]).to be_an_instance_of(Coin)
+    end
+  end
+
+  describe '#release_coin' do
+    it 'calls the coin\'s release method' do
+      puts 'Huh, ReleaseCoin? That\'d be a terrible name for a crypto token.'
+      coin = spy('coin')
+      allow(coin).to receive(:value) { 1 }
+      mock_coins = [coin]
+      allow_any_instance_of(Change).to receive(:generate_coins) { mock_coins }
+      test_change = Change.new
+      test_change.release_coin(1, 1)
+      expect(coin).to have_received(:release).with(1)
     end
   end
 end
