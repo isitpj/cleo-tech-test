9c5bd13d76f99bb8e4ec280c0bb78a5bb5dfdd25
Peter Johnstone <peter.wdj@gmail.com>
2018-03-07 18:53:47 +0000

    Tests that #print_reload_option calls puts

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -11,4 +11,13 @@ describe Printer do
       expect(STDOUT).to have_received(:puts)
     end
   end
+
+  describe '#print_reload_option' do
+    it 'prints a string offering the user the option to reload the machine' do
+      allow(STDOUT).to receive(:puts)
+      printer = Printer.new
+      printer.print_reload_option
+      expect(STDOUT).to have_received(:puts)
+    end
+  end
 end
