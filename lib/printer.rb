c63f7a6d28415e580b408d40483f9823cd503f0f
Peter Johnstone <peter.wdj@gmail.com>
2018-03-07 18:53:22 +0000

    Adds #print_reload_option

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,4 +2,8 @@ class Printer
   def print_product(product, index)
     STDOUT.puts "#{index + 1}. #{product.name}. Price: #{product.price}"
   end
+
+  def print_reload_option
+    puts 'Please enter \'reload\' to add more products or change to the machine.'
+  end
 end
