b2db50c8bc47d32092dfe8218744997c0dc089ef
Peter Johnstone <peter.wdj@gmail.com>
2018-03-09 14:49:04 +0000

    Injects merchandise variable into new instance of Reload

--- lib/reload.rb
+++ lib/reload.rb
@@ -1,10 +1,12 @@
 require_relative './printer'
+require_relative './merchandise'
 
 class Reload
-  attr_reader :printer
+  attr_reader :printer, :merchandise
 
-  def initialize
+  def initialize(merchandise)
     @printer = Printer.new
+    @merchandise = merchandise
   end
 
   def assign_product_or_change
@@ -12,3 +14,26 @@ class Reload
     selection = STDIN.gets.chomp
   end
 end
+
+
+
+
+
+
+# def reload_product
+#   @printer.print_product_selection(@merchandise)
+#   product_index = STDIN.gets.chomp.to_i - 1
+#   @printer.request_quantity
+#   quantity = STDIN.gets.chomp.to_i
+#   @merchandise.reload_product(product_index, quantity)
+#   @printer.print_successful_reload
+# end
+#
+# def reload_coin
+#   @printer.request_coin_selection
+#   denomination = STDIN.gets.chomp.to_i
+#   @printer.request_quantity
+#   quantity = STDIN.gets.chomp.to_i
+#   @change.insert_coin(denomination, quantity)
+#   @printer.print_successful_reload
+# end
