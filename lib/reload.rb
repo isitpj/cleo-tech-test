e08d9ad500ab613540c5ec4b629c2e5fa96ee523
Peter Johnstone <peter.wdj@gmail.com>
2018-03-09 14:36:10 +0000

    Initializes Reload with an instance of Printer

--- lib/reload.rb
+++ lib/reload.rb
@@ -1,4 +1,12 @@
+require_relative './printer'
+
 class Reload
+  attr_reader :printer
+
+  def initialize
+    @printer = Printer.new
+  end
+
   def assign_product_or_change
     selection = STDIN.gets.chomp
   end
