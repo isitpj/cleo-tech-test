f251404ff68455302446c3d63f9db4628e9c1ae7
Peter Johnstone <peter.wdj@gmail.com>
2018-03-09 16:11:01 +0000

    Adds tests from Machine#accept_coins and modifies for Reload

--- spec/dispense_spec.rb
+++ spec/dispense_spec.rb
@@ -1,8 +1,13 @@
 require 'dispense'
 
 describe Dispense do
+  let(:dispense) { described_class.new(1, Merchandise.new, Change.new) }
+
+  before(:each) do
+    allow(STDOUT).to receive(:puts)
+  end
+
   describe '#initialize' do
-    let(:dispense) { described_class.new(1, Merchandise.new, Change.new) }
     it 'has a printer variable set to an instance of Printer' do
       expect(dispense.printer).to be_an_instance_of(Printer)
     end
@@ -21,6 +26,46 @@ describe Dispense do
 
     it 'has a change_due that is nil by default' do
       expect(dispense.change_due).to eq nil
+    end
+  end
+
+  describe '#accept_coins' do
+    before(:each) do
+      allow(STDIN).to receive(:gets).and_return('3', '50', '20', '20')
+    end
+
+    it 'calls gets to receive user input' do
+      dispense.accept_coins(90)
+      expect(STDIN).to have_received(:gets).at_least(2).times
+    end
+
+    it 'will not accept coins that are not a valid denomination' do
+      dispense.accept_coins(90)
+      expect(STDOUT).to have_received(:puts).with 'Sorry, that is not a valid denomination.'
+    end
+
+    it 'calls the Change class\'s insert_coin method for each coin inserted' do
+      merchandise = double('merchandise')
+      change = spy('change')
+      allow(Change).to receive(:new) { change }
+      test_dispense = Dispense.new(1, merchandise, change)
+      test_dispense.accept_coins(90)
+      expect(change).to have_received(:insert_coin).exactly(3).times
+    end
+
+    it 'returns 30p of change' do
+      allow(STDIN).to receive(:gets).and_return('3', '50', '20', '50')
+      expect(dispense.accept_coins(90)).to eq [20, 10]
+    end
+
+    it 'returns 40p of change' do
+      allow(STDIN).to receive(:gets).and_return('20', '20', '20', '20', '50')
+      expect(dispense.accept_coins(90)).to eq [20, 20]
+    end
+
+    it 'decreases the quantity of a coin stored' do
+      allow(STDIN).to receive(:gets).and_return('20', '20', '20', '20', '50')
+      expect { dispense.accept_coins(90) }.to change { dispense.change.coins[3].quantity }.from(20).to(22)
     end
   end
 end
