0625e8cbaf5bcabbd61ad80d0a188a4f8af72a78
Peter Johnstone <peter.wdj@gmail.com>
2018-03-09 14:36:28 +0000

    Tests that Reload#printer is an instance of Printer

--- spec/reload_spec.rb
+++ spec/reload_spec.rb
@@ -1,6 +1,12 @@
 require 'reload'
 
 describe Reload do
+  describe '#initialize' do
+    it 'has a @printer variable that is an instance of Printer' do
+      reload = Reload.new
+      expect(reload.printer).to be_an_instance_of(Printer)
+    end
+  end
   describe '#assign_product_or_change' do
     it 'calls gets to receive user input' do
       reload = Reload.new
