9478a2ed445907e2102b43ab18e64e85fbc23881
Peter Johnstone <peter.wdj@gmail.com>
2018-03-09 14:37:25 +0000

    Refactors reload.new to let block

--- spec/reload_spec.rb
+++ spec/reload_spec.rb
@@ -1,15 +1,16 @@
 require 'reload'
 
 describe Reload do
+  let(:reload) { described_class.new }
+
   describe '#initialize' do
     it 'has a @printer variable that is an instance of Printer' do
-      reload = Reload.new
       expect(reload.printer).to be_an_instance_of(Printer)
     end
   end
+
   describe '#assign_product_or_change' do
     it 'calls gets to receive user input' do
-      reload = Reload.new
       allow(STDIN).to receive(:gets) { 'change' }
       reload.assign_product_or_change
       expect(STDIN).to have_received(:gets)
