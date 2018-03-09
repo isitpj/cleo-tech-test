ad7103711964c772bf169d6060eab5dac79e7b3a
Peter Johnstone <peter.wdj@gmail.com>
2018-03-09 15:12:35 +0000

    Calls #reload_coin from #assign_product_or_change

--- spec/reload_spec.rb
+++ spec/reload_spec.rb
@@ -20,22 +20,27 @@ describe Reload do
   describe '#assign_product_or_change' do
     before(:each) do
       allow(STDIN).to receive(:gets) { 'change' }
+      allow(STDOUT).to receive(:puts)
     end
 
     it 'calls the Printer class\'s #print_reload_options method' do
       printer = spy('printer')
       allow(Printer).to receive(:new) { printer }
+      allow_any_instance_of(Change).to receive(:insert_coin)
       reload.assign_product_or_change
       expect(printer).to have_received(:print_reload_options)
     end
 
     it 'calls gets to receive user input' do
+      allow(reload).to receive(:reload_coin)
       reload.assign_product_or_change
       expect(STDIN).to have_received(:gets)
     end
 
-    xit 'calls #reload_coin' do
-
+    it 'calls #reload_coin' do
+      allow(reload).to receive(:reload_coin)
+      reload.assign_product_or_change
+      expect(reload).to have_received(:reload_coin)
     end
   end
 
